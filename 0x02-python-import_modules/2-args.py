fe24a33407ef943627971b54baa7fdb3ee48d5d6
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-06-08 11:06:58 -0700

    added the if name == main check

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
+if __name__ =='__main__':
+    import sys
+    words = sys.argv[1:]
+    words_count = len(words)
 
-import sys
-words = sys.argv[1:]
-words_count = len(words)
-if words_count == 0:
-    print(f"{words_count} arguments.")
-elif words_count == 1:
-     print(f"{words_count} argument.")
-else:
-     print(f"{words_count} arguments.")
+    if words_count == 0:
+        print(f"{words_count} arguments.")
+    elif words_count == 1:
+         print(f"{words_count} argument.")
+    else:
+         print(f"{words_count} arguments.")
 
-for i in range (words_count):
-    position = i + 1
-    word = words[i]
-    print(position, ":", word)
+    for i in range (words_count):
+        index = i + 1
+        word = words[i]
+        print(index, ":", word)
