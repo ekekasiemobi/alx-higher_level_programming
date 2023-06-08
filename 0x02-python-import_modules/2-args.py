cad148a8b34cc944178f65df30ba9572d87a45f3
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-06-08 11:25:49 -0700

    fixed pycodestyle error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-if __name__ =='__main__':
+if __name__ == '__main__':
     import sys
     words = sys.argv[1:]
     words_count = len(words)
@@ -7,11 +7,11 @@ if __name__ =='__main__':
     if words_count == 0:
         print(f"{words_count} arguments.")
     elif words_count == 1:
-         print(f"{words_count} argument.")
+        print(f"{words_count} argument.")
     else:
-         print(f"{words_count} arguments.")
+        print(f"{words_count} arguments.")
 
-    for i in range (words_count):
+    for i in range(words_count):
         index = i + 1
         word = words[i]
         print(index, ":", word)
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,8 +3,8 @@ if __name__ == '__main__':
     import sys
     sum = 0
     num = sys.argv[1:]
-    num_count= len(num)
+    num_count = len(num)
 
     for x in range(num_count):
-      sum = sum + int(num[x])
+        sum = sum + int(num[x])
     print(sum)
