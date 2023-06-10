4d869795ceab50b56acf023539af370bfefef8fd
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-06-10 07:41:25 -0700

    added space around operator

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,6 +5,6 @@ def print_matrix_integer(matrix=[[]]):
         item_count = len(items)
         for item in range(item_count):
             print("{:d}".format(items[item]), end="")
-            if item < item_count -1:
+            if item < item_count - 1:
                 print(" ", end="")
         print("")
