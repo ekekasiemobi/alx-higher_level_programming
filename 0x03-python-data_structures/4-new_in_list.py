7029ca3fc9398f04d0ce9f153c6b1b7df8210d42
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-06-10 06:52:10 -0700

    made a copy of my_list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,9 +2,7 @@
 
 def new_in_list(my_list, idx, element):
     list_count = len(my_list)
-    if idx < 0:
-        return my_list
-    if idx > list_count:
+    if idx < 0 and idx > list_count:
         return my_list
     else:
         new_list = my_list.copy()
