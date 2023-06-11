30b0c58643046dc87beb8396ec2f0d91a0a6cf60
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-06-11 16:59:14 -0700

    fixed pycodestyle error

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,10 +2,7 @@
 
 def element_at(my_list, idx):
     list_count = len(my_list)
-    if idx < 0:
-        return None
-    if idx > list_count:
+    if idx < 0 or idx >= list_count:
         return None
     else:
         return idx
-
