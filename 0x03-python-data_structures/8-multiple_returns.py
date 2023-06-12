dd879ebf37251c08cae87316e5183047ee7ebe99
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-06-12 16:54:28 -0700

    removed the for loop

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,9 +2,8 @@
 
 def multiple_returns(sentence):
     length = len(sentence)
-    for letters in sentence:
-        if length == 0:
-            return (0, None)
-        else:
-            first = sentence[0]
-            return (length, first)
+    if length == 0:
+        return (0, None)
+    else:
+        first = sentence[0]
+        return (length, first)
