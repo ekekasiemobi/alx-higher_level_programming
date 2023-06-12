370518b35cf3ce5d5d6d69ca8b69c70260b40914
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-06-12 16:42:50 -0700

    added the copy() function

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+
 def print_reversed_list_integer(my_list=[]):
-    for num in my_list:
-        print("{}".format(my_list[-num]))
+    if isinstance(my_list, list):
+        my_list.reverse()
+        for item in my_list:
+            print('{:d}'.format(item))
