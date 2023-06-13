72ac1b39f1b17e0213fd6d416d62387e4615ee80
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-06-12 17:05:37 -0700

    added = to the loop

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 
 def delete_at(my_list=[], idx=0):
     list_count = len(my_list)
-    if idx < 0 or idx > list_count:
+    if idx < 0 or idx >= list_count:
         return my_list
     else:
         del my_list[idx]
