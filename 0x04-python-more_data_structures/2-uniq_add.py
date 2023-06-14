45de57c1e32ade3775b7e23ae25790a489de93e2
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-06-14 06:17:56 -0700

    fixed pycodestyle error

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -4,8 +4,8 @@ def uniq_add(my_list=[]):
     result = []
     sum = 0
     for i in my_list:
-      if i not in result:
-        sum += i
-        result.append(i)
-            
+        if i not in result:
+            sum += i
+            result.append(i)
+
     return sum
