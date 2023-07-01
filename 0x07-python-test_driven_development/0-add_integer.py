f00ac01667fccbad986814ffe51324671bc86bf3
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-07-01 05:35:15 -0700

    0-task compiled successfully

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,9 +2,9 @@
 
 def add_integer(a, b=98):
 
-    if type(a) is not (int, float):
+    if type(a) not in (int, float):
         raise TypeError("a must be an integer")
-    if type(b) is not (int, float):
+    if type(b) not in (int, float):
         raise TypeError("b must be an integer")
 
     if type(a) == float:
