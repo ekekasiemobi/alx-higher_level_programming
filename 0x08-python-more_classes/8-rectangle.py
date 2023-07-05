a56dfb39d7ffe33b906aa6ec62340801e1acf30d
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-07-05 08:28:57 -0700

    add the number of instance in init

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -15,6 +15,7 @@ class Rectangle:
         """initializes private instance width and height"""
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -77,7 +77,7 @@ class Rectangle:
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
 
-   @staticmethod
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """returns the rectangle based on biggest area"""
         if type(rect_1) is not Rectangle:
