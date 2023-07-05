793c0670052f17ef714addabb9881d01c9a88295
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-07-05 08:12:11 -0700

    returns same value of rect_1 and rect_2

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -79,16 +79,11 @@ class Rectangle:
 
    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        """
-            method to return the bigger of 2 rectangles
-        """
-        if not isinstance(rect_1, Rectangle):
+        """returns the rectangle based on biggest area"""
+        if type(rect_1) is not Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
-
-        if not isinstance(rect_2, Rectangle):
+        if type(rect_2) is not Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
-
         if rect_1.area() >= rect_2.area():
             return rect_1
-        else:
-            return rect_2
+        return rect_2
