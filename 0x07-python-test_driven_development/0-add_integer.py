012ab505a009225c456203317d8abd6bc12e53b1
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-07-07 04:51:46 -0700

    fixed the requirement error

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+"""
+adds two integer
+and returns an integer
+
+"""
 
 def add_integer(a, b=98):
+    """
+    argument must be an int else raise a type error
+    """
 
     if type(a) not in (int, float):
         raise TypeError("a must be an integer")
