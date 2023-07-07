66837ce6446066b7bb49c9fb9cdd44df0a79e343
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-07-07 08:20:23 -0700

    unite test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -25,5 +25,35 @@ class TestMaxInteger(unittest.TestCase):
         with self.assertRaises(TypeError):
             max_integer(None)
 
+    def test_one_negative(self):
+        """Tests list with one negative number"""
+        my_list = [20, 80, 30, -40, 10, 50]
+        self.assertEqual(max_integer(my_list), 80)
+
+    def test_empty_list(self):
+        """Tests for empty list []"""
+        my_list = []
+        self.assertIsNone(max_integer(my_list))
+
+    def test_max_beginning(self):
+        """Test positive value with max at the beginning"""
+        my_list = [80, 60, 10, 40, 30, 50]
+        self.assertEqual(max_integer(my_list), 80)
+
+     def test_max_end(self):
+        """Tests positive with max at the end"""
+        my_list = [50, 30, 10, 40, 60, 80]
+        self.assertEqual(max_integer(my_list), 80)
+
+    def test_max_middle(self):
+        """Tests positive with max in the middle"""
+        my_list = [60, 10, 40, 80, 30, 50]
+        self.assertEqual(max_integer(my_list), 80)
+
+    def test_all_negative(self):
+        """Tests list with all negative values"""
+        my_list = [-60, -30, -40, -10, -80]
+        self.assertEqual(max_integer(my_list), -10)
+
 if __name__ == "__main__":
     unittest.main()
