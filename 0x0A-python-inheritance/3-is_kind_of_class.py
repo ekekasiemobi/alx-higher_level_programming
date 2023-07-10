033fd3da4374d8d738cba492610abf9297fe7c27
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-07-10 06:59:37 -0700

    returns obj and int

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -2,10 +2,10 @@
 """This creates a function thats returns true with prototype is_kind_of_class(obj)"""
 
 
-def is_type_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """
         Function to return True if an object is exactly an instance
         of a specified clase else False
     """
 
-    return type(obj) is a_class
+    return isinstance(obj, a_class)
