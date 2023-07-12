5adbaa5a83bddb243fb3f93a8fb8eb92a36deb10
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-07-12 05:51:42 -0700

    creates the function

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Add all arguments and save them to a file"""
+
+import sys
+load_from_json_file = __import__('6-load_from_json_file', fromlist=['load_from_json_file']).load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file', fromlist=['save_to_json_file']).save_to_json_file
+
+try:
+    arguments = load_from_json_file("add_item.json")
+    if arguments is None:
+        save_to_json_file(sys.argv[1:], "add_item.json")
+    else:
+        arguments.extend(sys.argv[1:])
+        save_to_json_file(arguments, "add_item.json")
+except FileNotFoundError:
+    save_to_json_file(sys.argv[1:], "add_item.json")
+
