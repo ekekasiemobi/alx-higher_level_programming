472d3e952c70517d6f9e879174e3948967582038
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-07-15 06:31:52 -0700

    creates the main function

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -64,3 +64,60 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+     def area(self):
+        """returns the area of the class"""
+        return self.width * self.height
+
+    def display(self):
+        """prints the dimension of class"""
+        [print() for v in range(self.y)]
+        for h in range(self.__height):
+            [print(" ", end="") for h in range(self.x)]
+            [print("#", end="") for w in range(self.width)]
+            print()
+
+     def __str__(self):
+        """Return the print() and str() representation of the Rectangle."""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                       self.x, self.y,
+                                                       self.width, self.height)
+
+     def update(self, *args, **kwargs):
+        """
+            updates propeties of a class instance
+            with args or kwargs if args is not passed
+        """
+        if len(args):
+            for i, a in enumerate(args):
+                if i == 0:
+                    self.id = a
+                elif i == 1:
+                    self.width = a
+                elif i == 2:
+                    self.height = a
+                elif i == 3:
+                    self.x = a
+                elif i == 4:
+                    self.y = a
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "width" in kwargs:
+                self.width = kwargs["width"]
+            if "height" in kwargs:
+                self.height = kwargs["height"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
+
+     def to_dictionary(self):
+        """Return the dict rep of a Rectangle."""
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
