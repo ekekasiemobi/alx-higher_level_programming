c20752e0c2333b3b41a7982ddfa54849097c8247
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-07-15 05:30:43 -0700

    loads from file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -35,3 +35,29 @@ class Base:
                 json_string = cls.to_json_string(list_dicts)
                 jsonfile.write(json_string)
 
+     @staticmethod
+     def from_json_string(json_string):
+         if json_string is None:
+            return "[]"
+        return json.loads(json_string)
+
+     @classmethod
+     def create(cls, **dictionary):
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                new = cls(1, 1)
+            else:
+                new = cls(1)
+            new.update(**dictionary)
+            return new
+
+    @classmethod
+    def load_from_file(cls):
+        """returns a list of instances"""
+        new_file = cls.__name__ + ".json"
+        try:
+            with open(new_file, "r") as f:
+                lst = cls.from_json_string(f.read())
+                return [cls.create(**d) for d in lst]
+        except Exception:
+            return []
