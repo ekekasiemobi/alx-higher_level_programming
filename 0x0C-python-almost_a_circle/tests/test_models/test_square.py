4657158c4ce63e6df1a80b363fa4f366928e451e
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-07-17 07:29:55 -0700

    creates the square unit test case

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -77,3 +77,60 @@ class TestSquare(unittest.TestCase):
     def test_float_y(self):
         with self.assertRaisesRegex(TypeError, "y must be an integer"):
             r = Rectangle(10, 4, 6.27)
+
+    def test_invalid_args(self):
+        with self.assertRaises(TypeError):
+            Rectangle(8, 10, 4, 8, 2, 1)
+
+    def test_less_args(self)
+        with self.assertRaises(TypeError):
+            Rectangle(8)
+            Rectangle()
+            Rectangle(None)
+
+    def test_class(self):
+        s = Square(10)
+        self.assertEqual(type(s), Square)
+
+    def test_area(self):
+        self.assertEqual(Square(5).area(), 25)
+        self.assertEqual(Square(5, 0, 0).area(), 25)
+
+    def test_update(self):
+        s = Square(5)
+        s.update(2)
+        self.assertEqual(s.id, 2)
+        s.update(2, 8)
+        self.assertEqual(s.size, 8)
+        s.update(2, 8, 4)
+        self.assertEqual(s.x, 4)
+        s.update(2, 8, 4, 6)
+        self.assertEqual(s.y, 6)
+        s.update(id=3)
+        self.assertEqual(s.id, 3)
+        s.update(size=10)
+        self.assertEqual(s.size, 10)
+        s.update(x=3, y=5)
+        self.assertEqual(s.x, 3)
+        self.assertEqual(s.y, 5)
+
+    def test_update_kwargs(self):
+        s = Square(1, 1, 1, 1)
+        with self.assertRaises(TypeError):
+            s.update(size="hello")
+        with self.assertRaises(TypeError):
+            s.update(x="hello")
+        with self.assertRaises(TypeError):
+            s.update(y="hello")
+        with self.assertRaises(ValueError):
+            s.update(size=-1)
+        with self.assertRaises(ValueError):
+            s.update(size=0)
+        with self.assertRaises(ValueError):
+            s.update(x=-1)
+        with self.assertRaises(ValueError):
+            s.update(y=-1)
+    def test_to_dictionary(self):
+        s = Square(10, 4, 6, 2)
+        expected_dict = {'id': 2, 'size': 10, 'x': 4, 'y': 6}
+        self.assertDictEqual(s.to_dictionary(), expected_dict)
