cd4c6ad517de29ae77f675a851e1b5c1f0c6d7d6
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-09-11 15:56:13 -0700

    fixes the trailing erorrs

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -9,8 +9,7 @@ if (args[2]) {
     for (i = 0; i < number; i++) {
       console.log('C is fun');
     }
-  } 
-  
+  }
 } else {
-   console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 }
