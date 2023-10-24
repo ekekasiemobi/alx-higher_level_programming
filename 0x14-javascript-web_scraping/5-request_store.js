1e971ea657c48d708a4ce73b8b3b5c9a6ab85715
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-10-24 10:59:06 -0700

    fixes requirement error

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,12 +5,12 @@ const url = process.argv[2];
 const path = process.argv[3];
 
 request(url, function (error, response, body) {
-  if (err) {
-      // pass
+  if (error) {
+    // pass
   } else {
     fs.writeFile(path, body, 'utf-8', function (err) {
       if (err) {
-          // pass
+        // pass
       }
     });
   }
