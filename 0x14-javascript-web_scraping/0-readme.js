1d5fc25897d0c8c4405b50931b8ab323e2aa3e40
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-10-24 09:46:57 -0700

    fixes reqirement error

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,13 +2,13 @@
 
 const fs = require('fs');
 
-const file = process.argv[2]
+const file = process.argv[2];
 
 fs.readFile(file, 'utf8', function (err, data) {
   if (err) {
     console.log(err);
     process.exit(1);
   } else {
-  console.log(data);
+    console.log(data);
   }
 });
